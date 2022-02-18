3b7e40621e43b4c08d13a72bbd3dd496c79d8aba
seo-0410 <wjddnjs2216@naver.com>
2022-02-18 17:26:01 +0900

    1

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print('aa')
+N, S = map(int, input().split())
+A = list(map(int, input().split()))
+
+stack = list()
+
+def GCD(a,b):
+    while b > 0:
+        a , b = b, a % b 
+    return a
+
+for i in A:
+    X = abs(i-S)
+    stack.append(X)
+
+
+dap = stack[0]
+for s_1 in range(1,len(stack)):
+    dap = GCD(stack[s_1],dap)
+    
+
+print(dap)
